45da61bdd0ecccf4488abaef06025ab16fa099db
CAROLINA VITÓRIA CHAVES SILVA <21276093@dsn127m1246731.cfp127aluno.local>
2022-09-27 16:00:48 -0300

    Ajustando menu; versão responsiva ok; ínicio dos componentes menores.

--- src/components/layout/Menu/Menu.js
+++ src/components/layout/Menu/Menu.js
@@ -1,29 +1,32 @@
-import {
-  HiOutlineHome,
-  HiOutlineDocumentText,
-  HiOutlineUser,
-} from "react-icons/hi";
-
 import styles from "./Menu.module.css";
 
 import IconLogo from "../../../components/Logo/IconLogo/IconLogo";
+import Home from "../../../assets/menuIcons/home.png";
+import List from "../../../assets/menuIcons/list.png";
+import Profile from "../../../assets/menuIcons/profile.png";
 
 const Menu = ({ icon, label }) => {
   return (
     <div className={styles.container_menu}>
-      <IconLogo />
+      <div className={styles.logo}>
+        <IconLogo />
+      </div>
 
       <nav className={styles.menu}>
         <div className={styles.menu_item}>
-          <HiOutlineHome size={30} color="#757575" />
+          <img src={Home} alt="Página inicial" title="Página inicial" />
           <p>Página inicial</p>
         </div>
-        <div className={styles.menu_item}>
-          <HiOutlineDocumentText size={30} color="#757575" />
+        <div className={`${styles.menu_item} ${styles.list}`}>
+          <img
+            src={List}
+            alt="Lista de hemocentros"
+            title="Lista de hemocentros"
+          />
           <p>Hemocentros</p>
         </div>
-        <div className={styles.menu_item}>
-          <HiOutlineUser size={30} color="#757575" />
+        <div className={`${styles.menu_item} ${styles.profile}`}>
+          <img src={Profile} alt="Perfil" title="Perfil" />
           <p>Perfil</p>
         </div>
       </nav>
