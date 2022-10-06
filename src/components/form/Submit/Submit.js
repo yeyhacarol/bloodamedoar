4a54526ed05f7d076d4cc3df9d127a5576619a55
Carolina <carolinav.chavesilva@gmail.com>
2022-10-06 13:04:40 -0300

    Ajustes.

--- src/components/form/Submit/Submit.js
+++ src/components/form/Submit/Submit.js
@@ -2,12 +2,21 @@ import { Link } from "react-router-dom";
 
 import styles from "./Submit.module.css";
 
-const Submit = ({ action, instruction, link, to, id, customClass }) => {
+const Submit = ({
+  action,
+  instruction,
+  link,
+  to,
+  id,
+  customClass,
+  handleOnClick,
+}) => {
   return (
     <div className={styles.button_container}>
       <button
         type="submit"
         className={`${styles.button} ${customClass}`}
+        onClick={handleOnClick}
         id={id}
       >
         {action}
