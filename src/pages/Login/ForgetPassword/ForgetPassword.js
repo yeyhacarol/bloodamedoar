96afeb454be07240e9b12641b5e566acc705597e
Carolina Vitória <carolinav.chavesila@gmail.com>
2022-11-14 11:38:38 -0300

    Integrando com serviço de envio de e-mail para recuperar senha.

--- src/pages/Login/ForgetPassword/ForgetPassword.js
+++ src/pages/Login/ForgetPassword/ForgetPassword.js
@@ -1,5 +1,7 @@
 import styles from "./ForgetPassword.module.css";
 
+import emailjs from "@emailjs/browser";
+
 import ModalLogo from "../../../components/logo/ModalLogo/ModalLogo";
 import Heading from "../../../components/Heading/Heading";
 import Input from "../../../components/form/Input/Input";
@@ -7,9 +9,58 @@ import Submit from "../../../components/form/Submit/Submit";
 
 import close from "../../../assets/bloobs/close.png";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 const ForgetPassword = () => {
   const navigate = useNavigate();
+
+  const [data, setData] = useState({
+    email: "",
+  });
+
+  const [error, setError] = useState({
+    email: {
+      error: false,
+      errorMessage: "",
+    },
+  });
+
+  const handleOnChange = (value, input) => {
+    setData((prevState) => ({ ...prevState, [input]: value }));
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (!data.email) {
+      setError({
+        email: {
+          error: true,
+          errorMessage: "Preencha este campo.",
+        },
+      });
+    }
+
+    const templateParams = {
+      email: data.email,
+    };
+
+    emailjs
+      .send(
+        "service_d7j679c",
+        "template_vw2im6p",
+        templateParams,
+        "siS2mdPzClHgjeTlw"
+      )
+      .then((response) => {
+        toast.success("Verifique seu e-mail.");
+        setData("");
+      })
+      .catch((error) => console.error(error));
+
+    console.log(data);
+  };
 
   return (
     <div className={styles.forgetpassword}>
@@ -32,12 +83,17 @@ const ForgetPassword = () => {
           realmente você que está tentando fazer login.
         </p>
 
-        <form>
+        <form onSubmit={handleOnSubmit}>
           <Input
             type="email"
             placeholder="Seu e-mail"
             label="Seu e-mail"
-            name="email" /* error={} errorMessage={} value={} handleOnChange={} */
+            name="email"
+            error={error.email.error}
+            errorMessage={error.email.errorMessage}
+            value={data.email || ""}
+            handleOnChange={handleOnChange}
+            onFocus={() => setError({ ...error, email: false })}
           />
           <Submit action="Confirmar" />
         </form>
